048397a64d39992df9e777e500b890ba31106dd5
martin-madsen <martin-madsen@m-u-4>
2025-09-13 15:42:55 +0100

    saCF

--- Src/Models/Server/V1/Initialize.php
+++ Src/Models/Server/V1/Initialize.php
@@ -25,32 +25,43 @@ abstract class Initialize extends CallBacks
 			$strConn	= $this->getProtocol() . "://". $this->getHost() .":" . $this->getPort() . "";
 			if ($this->getProtocol() ==  "tls") {
 				
-				$certObj	= $this->getCertificate();
-				//PEM formatted cert
-				$ssl		= stream_context_create();
-				
-				$fileObj	= \MTM\FS\Factories::getFiles()->getTempFile("pem")->setContent($certObj->getChainAsString());
-				stream_context_set_option($ssl, "ssl", "local_cert", $fileObj->getPathAsString());
-				
-				$fileObj	= \MTM\FS\Factories::getFiles()->getTempFile("pem")->setContent($certObj->getPrivateKey()->get());
-				stream_context_set_option($ssl, "ssl", "local_pk", $fileObj->getPathAsString());
-				
-				if ($certObj->getPrivateKey()->getPassPhrase() !== null) {
-					stream_context_set_option($ssl, "ssl", "passphrase", $certObj->getPrivateKey()->getPassPhrase());
-				}
-				
-				stream_context_set_option($ssl, "ssl", "allow_self_signed", true);
-				stream_context_set_option($ssl, "ssl", "verify_peer", false);
-				
-				set_error_handler(array($this, "connectError"));
+				//cannot delete this dir until server is down as every client will need it
+				$tmpDir		= \MTM\FS\Factories::getDirectories()->getTempDirectory();
 				try {
-					$sockRes	= stream_socket_server($strConn, $errno, $errstr, STREAM_SERVER_BIND|STREAM_SERVER_LISTEN, $ssl);
-					if (is_resource($sockRes) === true) {
-						stream_socket_enable_crypto($sockRes, false);
+					
+					$certObj	= $this->getCertificate();
+					//PEM formatted cert
+					$ssl		= stream_context_create();
+					
+					$certFile	= \MTM\FS\Factories::getFiles()->getTempFile("pem", $tmpDir)->setContent($certObj->getChainAsString());
+					stream_context_set_option($ssl, "ssl", "local_cert", $certFile->getPathAsString());
+					
+					$keyFile	= \MTM\FS\Factories::getFiles()->getTempFile("pem", $tmpDir)->setContent($certObj->getPrivateKey()->get());
+					stream_context_set_option($ssl, "ssl", "local_pk", $keyFile->getPathAsString());
+					
+					if ($certObj->getPrivateKey()->getPassPhrase() !== null) {
+						stream_context_set_option($ssl, "ssl", "passphrase", $certObj->getPrivateKey()->getPassPhrase());
 					}
-					restore_error_handler();
+					
+					stream_context_set_option($ssl, "ssl", "allow_self_signed", true);
+					stream_context_set_option($ssl, "ssl", "verify_peer", false);
+					
+					set_error_handler(array($this, "connectError"));
+					try {
+						
+						$sockRes	= stream_socket_server($strConn, $errno, $errstr, STREAM_SERVER_BIND|STREAM_SERVER_LISTEN, $ssl);
+						if (is_resource($sockRes) === true) {
+							stream_socket_enable_crypto($sockRes, false);
+						}
+						restore_error_handler();
+
+					} catch (\Exception $e) {
+						restore_error_handler();
+						throw $e;
+					}
+					
 				} catch (\Exception $e) {
-					restore_error_handler();
+					$tmpDir->delete();
 					throw $e;
 				}
 			} else {
@@ -65,13 +76,11 @@ abstract class Initialize extends CallBacks
 				}
 				
 			}
-			
+		
 			if (is_resource($sockRes) === true) {
-				
 				@stream_set_blocking($sockRes, false);
 				$this->_wsSock	= $sockRes;
 				$this->_isInit	= true;
-				
 			} else {
 				//if you get error: Address already in use, know that if the port was in use by another socket
 				//that is now shutdown, it will take a few seconds before the port is available again
